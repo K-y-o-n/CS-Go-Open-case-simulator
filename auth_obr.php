2c308ba7e1f1f04ce3b87662fa55c7cb260e963b
Петр Хомченко <70071692+K-y-o-n@users.noreply.github.com>
2020-11-19 21:00:59 +0300

    Update auth_obr.php

--- auth_obr.php
+++ auth_obr.php
@@ -9,7 +9,6 @@ $password = $_POST["password"];
 include("db.php");
 
 $req = $db->query("SELECT * FROM gameUser WHERE login='$login' AND password='$password'");
-//"SELECT * FROM gameUser LEFT JOIN "$login" ON admin.id_user = user.id WHERE email='$email' AND password='$password'"
 $user = $req->fetch_assoc();
 if($user){
   $_SESSION['user'] = $user;
